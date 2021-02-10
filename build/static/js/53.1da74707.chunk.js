17d69dbe08d96d89c4a54ba1a0c6798d4a515b62
Justin <justin.luczak@hotmail.fr>
2021-02-10 18:30:55 +0100

    Deploiment android studio

---
+++ build/static/js/53.1da74707.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptodolist=this.webpackJsonptodolist||[]).push([[53],{137:function(t,n,o){"use strict";o.r(n),o.d(n,"startStatusTap",(function(){return i}));var e=o(12),i=function(){var t=window;t.addEventListener("statusTap",(function(){Object(e.h)((function(){var n=t.innerWidth,o=t.innerHeight,i=document.elementFromPoint(n/2,o/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(e.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.1da74707.chunk.js.map
